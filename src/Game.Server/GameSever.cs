11ca130644e6d78b7a621f926dafbee02f5dab26
Dan Chartier <danchart@gmail.com>
2020-10-24 18:38:21 -0700

    UW

--- src/Game.Server/GameSever.cs
+++ src/Game.Server/GameSever.cs
@@ -1,22 +1,21 @@
 ﻿using Common.Core;
 using Game.Simulation.Server;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 
 namespace Game.Server
 {
     public class GameServer
     {
-        private List<SpawnedWorld> SpawnedWorlds = new List<SpawnedWorld>();
+        private SpawnedWorld[] SpawnedWorlds;
+        private ushort _worldCount;
+        private ushort[] _freeWorldIds;
+        private ushort _freeWorldCount;
 
         private PlayerConnectionManager _playerConnectionManager;
 
         private readonly ILogger _logger;
         private readonly IServerConfig _serverConfig;
-
-        private ushort _nextWorldId;
 
         public GameServer(
             IServerConfig serverConfig,
@@ -26,12 +25,16 @@ namespace Game.Server
             this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
             this._playerConnectionManager = new PlayerConnectionManager(this._serverConfig.Replication, this._serverConfig.PlayerConnection);
-            this._nextWorldId = 0;
+
+            this.SpawnedWorlds = new SpawnedWorld[8];
+            this._freeWorldIds = new ushort[8];
+            this._worldCount = 0;
+            this._freeWorldCount = 0;
         }
 
         public bool IsRunning()
         {
-            foreach (var spawnedWorld in this.SpawnedWorlds)
+            foreach (var spawnedWorld in this)
             {
                 if (spawnedWorld.Thread.ThreadState != ThreadState.Stopped)
                 {
@@ -44,7 +47,7 @@ namespace Game.Server
 
         public void StopAll()
         {
-            foreach (var spawnedWorld in this.SpawnedWorlds)
+            foreach (var spawnedWorld in this)
             {
                 spawnedWorld.World.Stop();
             }
@@ -52,50 +55,79 @@ namespace Game.Server
 
         public uint SpawnWorld()
         {
+            ushort worldId;
+
+            if (this._freeWorldCount > 0)
+            {
+                worldId = this._freeWorldIds[this._freeWorldCount--];
+            }
+            else
+            {
+                if (this._worldCount == this.SpawnedWorlds.Length)
+                {
+                    Array.Resize(ref this.SpawnedWorlds, 2 * this._worldCount);
+                }
+
+                worldId = this._worldCount++;
+            }
+
             var world = new GameWorld(
-                NextWorldId(), 
+                worldId, 
                 this._logger,
                 this._serverConfig,
                 this._playerConnectionManager);
             var thread = new Thread(world.Run);
             thread.Start();
 
-            this.SpawnedWorlds.Add(
-                new SpawnedWorld
-                {
-                    Thread = thread,
-                    World = world
-                });
+            this.SpawnedWorlds[worldId].World = world;
+            this.SpawnedWorlds[worldId].Thread = thread;
 
             this._logger.Info($"Spawning world: id={world.Id:x8}, threadId={thread.ManagedThreadId}");
 
             return world.Id;
         }
 
-        private ushort NextWorldId()
+        public Enumerator GetEnumerator()
         {
-            if (this.SpawnedWorlds.Count == ushort.MaxValue)
-            {
-                var message = $"Tried to spawn more than maximum allowed number of worlds. Count={this.SpawnedWorlds.Count}";
-
-                _logger.Error(message);
-
-                throw new InvalidOperationException(message);
-            }
-
-            while (
-                this.SpawnedWorlds
-                .Where(
-                    x => x.World.Id == this._nextWorldId)
-                .Any())
-            {
-                this._nextWorldId++;
-            }
-
-            return this._nextWorldId++;
+            return new Enumerator(this);
         }
 
-        private struct SpawnedWorld
+        public struct Enumerator
+        {
+            private GameServer _gameServer;
+            private int _current;
+            private int _freeIndex;
+
+            public Enumerator(GameServer gameServer)
+            {
+                this._gameServer = gameServer;
+                this._current = -1;
+                this._freeIndex = 0;
+            }
+
+            public ref SpawnedWorld Current
+            {
+                get => ref this._gameServer.SpawnedWorlds[this._current];
+            }
+
+            public bool MoveNext()
+            {
+                this._current++;
+
+                while (
+                    this._current < this._gameServer._worldCount &&
+                    this._freeIndex < this._gameServer._freeWorldCount &&
+                    this._current == this._gameServer._freeWorldIds[this._freeIndex])
+                {
+                    this._freeIndex++;
+                    this._current++;
+                }
+
+                return _current < this._gameServer._worldCount;
+            }
+        }
+
+        public struct SpawnedWorld
         {
             public GameWorld World;
             public Thread Thread;
